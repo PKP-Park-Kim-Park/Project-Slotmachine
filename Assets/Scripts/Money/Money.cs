9f0a17650e290b46e5fdfd1a413a0a9382ee33c9
doncici77 <pkh2956@naver.com>
2025-08-27 12:09:21 +0900

    머니 클래스 구현, 맵 프리팹화
    클래스 다이어그램을 토대로 구현 하였음.
    유니티의 함수를 사용하지 않을것 같아서 MonoBehavier가 아니라 일반 클래스로 구현 하였음.

---
+++ Assets/Scripts/Money/Money.cs
@@ -0,0 +1,50 @@
+﻿public class Money
+{
+    public int _token { get; private set; }
+    public int _gold { get; private set; }
+    private const int CONVERT_SIZE = 100_000;
+
+    public Money(int startGold = 0, int startToken = 0)
+    {
+        _gold = startGold;
+        _token = startToken;
+    }
+
+    public void AddGold(int amount)
+    {
+        _gold += amount;
+    }
+
+    public bool SpendGold(int amount)
+    {
+        if (amount > 0 && _gold >= amount)
+        {
+            _gold -= amount;
+            return true;
+        }
+        return false; // 골드 부족
+    }
+
+    public void AddToken(int amount)
+    {
+        _token += amount;
+    }
+
+    public bool SpendToken(int amount)
+    {
+        if (amount > 0 && _token >= amount)
+        {
+            _token -= amount;
+            return true;
+        }
+
+        return false; // 토큰 부족
+    }
+
+    public void ConvertToken()
+    {
+        _token += _gold / CONVERT_SIZE;
+
+        _gold = 0;
+    }
+}
---
+++ Assets/Scripts/PKH/Test.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private Money money;
+
+    void Start()
+    {
+        money = new Money();
+        Debug.Log("new Money() 골드: " + money._gold + ", 토큰: " + money._token);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q) == true)
+        {
+            money.AddGold(50_000);
+            Debug.Log("AddGold 골드: " + money._gold + ", 토큰: " + money._token);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) == true)
+        {
+            bool a = money.SpendGold(30_000);
+            Debug.Log("SpendGold 골드: " + money._gold + ", 토큰: " + money._token);
+            if(a == false)
+            {
+                Debug.Log("골드 부족");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) == true)
+        {
+            money.AddToken(2);
+            Debug.Log("AddToken 골드: " + money._gold + ", 토큰: " + money._token);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) == true)
+        {
+            bool a = money.SpendToken(1);
+            Debug.Log("SpendToken 골드: " + money._gold + ", 토큰: " + money._token);
+            if (a == false)
+            {
+                Debug.Log("토큰 부족");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.T) == true)
+        {
+            money.ConvertToken();
+            Debug.Log("ConvertToken 골드: " + money._gold + ", 토큰: " + money._token);
+        }
+    }
+}

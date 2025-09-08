f62399d44a1f0ab55917c8fe6d116f998bcfb1a6
doncici77 <pkh2956@naver.com>
2025-09-08 16:40:34 +0900

    UseItem 클래스 수정

--- Assets/Scripts/Item/UseItem.cs
+++ Assets/Scripts/Item/UseItem.cs
@@ -1,59 +1,90 @@
-public abstract class ItemEffectBase
+﻿using System;
+using UnityEngine;
+
+public struct SymbolEffectData
 {
-    public int amount;
+    public FlagSymbols Symbols;
+    public float Amount;
+    public UseType OriginalUseType;
 }
 
-public class SymbolEffect : ItemEffectBase
+public struct PatternEffectData
 {
-    public FlagSymbols symbols;
+    public FlagPatterns Patterns;
+    public float Amount;
 }
 
-public class PatternEffect : ItemEffectBase
+public struct StressEffectData
 {
-    public FlagPatterns patterns;
-}
-
-public class StressEffect : ItemEffectBase
-{
-    public StressType stressType;
+    public StressType StressType;
+    public float Amount;
 }
 
 public class UseItem
 {
-    public void Use(ItemDataModel itemDataModel)
+    public event Action<SymbolEffectData> OnSymbolEffectReady;
+    public event Action<PatternEffectData> OnPatternEffectReady;
+    public event Action<StressEffectData> OnStressEffectReady;
+
+    public void Use(ItemDataModel model)
     {
-        switch(itemDataModel.hasRisk)
+        Debug.Log($"--- 아이템 사용 시작: {model.name} ---");
+
+        bool hasGoodEffect = model.hasRisk == HasRisk.Good || model.hasRisk == HasRisk.Both;
+        bool hasRiskEffect = model.hasRisk == HasRisk.Risk || model.hasRisk == HasRisk.Both;
+
+        if (hasGoodEffect)
         {
-            case HasRisk.Good :
-                
-                break;
-            case HasRisk.Risk :
+            CheckEffect(model.itemEffect);
+        }
 
-                break;
-            case HasRisk.Both :
+        if (hasRiskEffect)
+        {
+            CheckEffect(model.itemRiskEffect);
+        }
 
-                break;
-            default:
-                break;
+        if (!hasGoodEffect && !hasRiskEffect)
+        {
+            Debug.Log("적용할 효과가 없는 아이템입니다.");
         }
     }
 
-    public void CheckUseType(ItemDataModel itemDataModel)
+    private void CheckEffect(ItemEffectModel effectModel)
     {
-        switch (itemDataModel.itmeEffect.useType)
+        switch (effectModel.useType)
         {
             case UseType.Symbol:
-
-                break;
             case UseType.SymbolReward:
-
+                SymbolEffectData symbolData = new SymbolEffectData
+                {
+                    Symbols = effectModel.flagSymbols,
+                    Amount = effectModel.amount,
+                    OriginalUseType = effectModel.useType
+                };
+                Debug.Log($"[효과 적용] 심볼 | 타입: {symbolData.OriginalUseType}, 대상: {symbolData.Symbols}, 값: {symbolData.Amount}");
+                OnSymbolEffectReady?.Invoke(symbolData);
                 break;
+
             case UseType.PartternReward:
-
+                PatternEffectData patternData = new PatternEffectData
+                {
+                    Patterns = effectModel.flagPatterns,
+                    Amount = effectModel.amount
+                };
+                Debug.Log($"[효과 적용] 패턴 | 대상: {patternData.Patterns}, 값: {patternData.Amount}");
+                OnPatternEffectReady?.Invoke(patternData);
                 break;
+
             case UseType.Stress:
-
+                StressEffectData stressData = new StressEffectData
+                {
+                    StressType = effectModel.stressType,
+                    Amount = effectModel.amount
+                };
+                Debug.Log($"[효과 적용] 스트레스 | 대상: {stressData.StressType}, 값: {stressData.Amount}");
+                OnStressEffectReady?.Invoke(stressData);
                 break;
+
             default:
                 break;
         }
